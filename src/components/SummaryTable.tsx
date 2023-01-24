b901b43a9fe57dc0deaf6b86c4358415228df4b9
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-01-24 10:26:20 -0300

    feat: generating days as the current day

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,6 +1,11 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+console.log(summaryDates);
 
 export function SummaryTable() {
   return (
@@ -19,7 +24,9 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
       </div>
     </div>
   );
