46ee59b52e321133bd529980de500f72ac808224
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-01-24 00:30:53 -0300

    feat: creating header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./styles/global.css";
-import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
 
-function App() {
-  return <Habit completed={3} />;
+// import { Habit } from "./components/Habit";
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
+  );
 }
-
-export default App;
