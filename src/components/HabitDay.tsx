495230a1d12c37b868f75749a0baaf8cd7d279f5
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-01-24 10:30:25 -0300

    fix: removing unnecessary div

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -4,8 +4,6 @@
 
 export function HabitDay() {
   return (
-    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center">
-      <div className="h-10 w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-    </div>
+    <div className="h-10 w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
   );
 }
